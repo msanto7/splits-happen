b26636fc0ac63f6d3be10af85f70b3624211aaf7
MIKE <mike.santoro42@gmail.com>
2018-10-30 17:50:27 -0400

    Move Game instance out to be visible by helper method (unfinished)

--- test/TestScore.java
+++ test/TestScore.java
@@ -16,7 +16,7 @@ public class TestScore extends TestCase{
 		//game of 0 (all gutter balls)
 		public void testZero() {
 			BowlingScore game = new BowlingScore();  //create game instance
-			for (int i = 0; i <=20; i++) {
+			for (int i = 0; i < 20; i++) {
 				game.throwBall(0);
 			} //ends for
 			
@@ -26,14 +26,23 @@ public class TestScore extends TestCase{
 		
 		//game of no spares or strikes....just empty a few pins each roll
 		public void testOne() {
+			int maxRolls = 20;  //need to remove hardcode rolls 
+			int numPins = 0;
 			BowlingScore game = new BowlingScore();
-			for (int i = 0; i <= 20; i++) {
-				game.throwBall(1);
-			} //ends for
+			rollRepeat(maxRolls, numPins);
 			
 			assertEquals(20, game.score());
 		}
+		
+			//helper
+		private void rollRepeat (int numRolls, int numPins) {
+			for (int i = 0; i < numRolls; i++) {
+				game.roll(numPins);
+				//need to make game visible to this method
+			} // ends for
+		}
 		//***********************************************************
+		
 		
 		
 	
