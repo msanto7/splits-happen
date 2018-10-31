b403725e366c6cab08d96c70bee4f9970bb1603d
MIKE <mike.santoro42@gmail.com>
2018-10-31 10:43:52 -0400

    Code and comments cleaned (All tests Passing)

--- src/BowlingScore.java
+++ src/BowlingScore.java
@@ -7,45 +7,14 @@ public class BowlingScore {
 
 	// 10 frames
 	// 10 pins per frame
-	// 2 rolls in every frame besides the final (if strike the 2, if spare then 3)
+	// 2 rolls in every frame besides the final
 	private int rollsArray[] = new int[21]; // 21 max rolls
 	private int currentRoll = 0; // this will help keep track of frames and rolls
-	// int score = 0; //global initialize
-
-	public static void main(String[] args) {
-		// Main Driver for bowling score program
-		// so we are provided with a valid sequence of rolls from the begininng
-		// must decide on how to store
-		// the sequence will contain numbers 1-9, "/" (spare), "X" (strike), "-" (miss)
-		// how should we store a sequence?
-		// array?...we need to conceptualize a frame in addition to just individual
-		// rolls..
-		// this is how we will keep track of a spare or strike
-		// we need to keep track of 2 frames at a time...only adding to the total score
-		// when we are sure we have finished calulcating a spare or strike scenario
-
-		System.out.println("Bowling Score:  "); // Build test
-
-	} // End Main
 
 	// Scoring Logic
 	// ************************************************************************
 	public int score() {
 		// calculate and return score
-
-		// initialize
-		// int score = 0; brought up one level to give other methods access to variable
-		// for (int i = 0; i < 21; i++) {
-		// if frame total < 10
-		// basic case (frame is neither spare nor strike
-		// score = (roll1 of frame) + (roll2 of frame)
-		// else if ('/' spare)
-		// score = 10 + (next roll)
-		// else ('X' strike)
-		// score = 10 + (next 2 rolls, total of pins down in the next frame)
-		// } End for loop
-		// return 0; used for original hardcoded solution
-
 		int score = 0;
 		int frameCursor = 0; // need to create an idea of a frame
 
@@ -59,50 +28,24 @@ public class BowlingScore {
 				score = score + 10 + rollsArray[frameCursor + 2];
 				frameCursor = frameCursor + 2;
 			} else {
-				score = score + rollsArray[frameCursor] + rollsArray[frameCursor + 1]; // add the 2 in a frame
-				frameCursor = frameCursor + 2; // meaning we need to jump 2 rolls into the next frame..we have fully
-												// processed the current one
+				score = score + rollsArray[frameCursor] + rollsArray[frameCursor + 1];
+				frameCursor = frameCursor + 2; // meaning we need to jump 2 rolls into the next frame
 			}
 		} // ends for
 
 		return score;
 
 	} // End Method Score
-		// ************************************
-
+	// ************************************
 	public void throwBall(int numPins) {
-		
-		
-		//XXXXXXXXXX There is where I may be able to account for the characters 
-		
-		//lets say we pass this method a char...
-		//and use each character to build an integer array
-		//so if we roll a ('X')...we populate the rollsArray with the integer 10
-		
-		//this way our logic is the same...and we process it before even entering the score 
-		
-		//we could also maybe overload the throwBall method...so if it recieves a character 
-		//we use another throwBall method that takes care of that arguement and correctly
-		//updates the rollsArray
-		
-		//XXXXXXXXXXX
-			
-
-		// score = score + numPins; //simple case without any logic for spare or strike
-		// move score outside single method scope
-
-		rollsArray[currentRoll++] = numPins; // so this updates the the frame score including the next roll (for the
-												// case of a spare)
+		rollsArray[currentRoll++] = numPins;
 	}
-	//*****
+	//***********************
 	public void throwBall(String gameInput) {
-		//String[] individualRolls = gameInput.split("[]+");
-		char[] individualRolls = gameInput.toCharArray();
-		
+		char[] individualRolls = gameInput.toCharArray();	
 		for (int i = 0; i < individualRolls.length; i++) {
-			System.out.println(individualRolls[i]);
 			
-			switch (individualRolls[i]) {
+			switch (individualRolls[i]) {     //this is ugly, better way to parse input?
 			case 'X':
 				rollsArray[i] = 10;
 				break;
@@ -110,6 +53,9 @@ public class BowlingScore {
 				rollsArray[i] = 10 - rollsArray[i-1];
 				break;
 			case '-':
+				rollsArray[i] = 0;
+				break;
+			case '0':
 				rollsArray[i] = 0;
 				break;
 			case '1':
@@ -138,13 +84,11 @@ public class BowlingScore {
 				break;
 			case '9':
 				rollsArray[i] = 9;
-				break;
-				
+				break;				
 			default: System.out.println("Invalid character");
-					
+				break;
 			} //end switch
-		} //end for
-		
+		} //end for	
 	}
 	// **********************************************************
 	public boolean isSpare(int frameCursor) {
@@ -160,12 +104,11 @@ public class BowlingScore {
 } // End Class
 
 //out of scope but will try to implement
-// checking for valid rolls
-// check for correct number of rolls and frames
-// provide scores for intermediate frames (this will probably be the most
-// intensive)
+	// checking for valid rolls
+	// check for correct number of rolls and frames
+	// provide scores for intermediate frames (this will probably be the most
+	// intensive)
 
 //Resources used towards the development design and solution 
+	// http://butunclebob.com/ArticleS.UncleBob.TheBowlingGameKata
 
-// https://www.youtube.com/watch?v=OPGTPQ4kURU
-//
--- test/TestScore.java
+++ test/TestScore.java
@@ -2,7 +2,6 @@
 import junit.framework.*; //junit added to project path
 
 public class TestScore extends TestCase {
-
 	private BowlingScore game;
 
 	// constructors
@@ -10,137 +9,81 @@ public class TestScore extends TestCase {
 		game = new BowlingScore();
 	}
 
-	// Junit tests to examine game behavior
-	// example test cases
-
-	// game of 0 (all gutter balls)
+	//Tests
 	public void testZero() {
-
-		throwBallRepeat(20, 0); // method used to make things cleaner and quicker than the for loop in every
-								// individual method
-		assertEquals(0, game.score()); // return 0 from score method
+		throwBallRepeat(20, 0);
+		assertEquals(0, game.score()); // check if score is accurate
 	}
-
-	// ************************************************************
+	// ************************************************
 	// game of no spares or strikes....just empty a few pins each roll
 	public void testOne() {
-		// int maxRolls = 20; //need to remove hardcode rolls
-		// int numPins = 0;
-		// BowlingScore game = new BowlingScore();
-
 		throwBallRepeat(20, 1);
 		assertEquals(20, game.score());
 	}
-
-	// ***********************************************************
+	// ************************************************
 	public void testSpareFollwedByAnyRoll() {
 		game.throwBall(5);
-		game.throwBall(5); // this would be a spare
-		game.throwBall(3); // so this would not affect previous frame score
-
+		game.throwBall(5);
+		game.throwBall(3);
 		throwBallRepeat(17, 0);
-
 		assertEquals(16, game.score());
 	}
-	// **********************************************************
-
+	// ************************************************
 	public void testStrikeFollwedByAnyRoll() {
 		game.throwBall(10);
 		game.throwBall(3);
 		game.throwBall(4);
 		throwBallRepeat(16, 0);
-
 		assertEquals(24, game.score());
 	}
-
-	// **********************************************************
+	// ***********************************************
 	// game of 300, perfect (all strikes)
 	public void testPerfect() {
 		throwBallRepeat(12, 10);
-
 		assertEquals(300, game.score());
 	}
-	// ********************************************************
-
-	// spare with next roll of zero
+	// ***********************************************
+	// spare with next roll of zero following
+	public void testSpareFollowedByMiss() {
+		game.throwBall("2/------------------");
+		assertEquals(10, game.score());
+	}
 	// spare with next roll of >= 1
+	public void testSpareFollowedByPins() {
+		game.throwBall("-/3-----------------");
+		assertEquals(16, game.score());
+	}
+	//spare in tenth frame followed by pins
+	public void testTenthFrameSpare() {
+		game.throwBall("------------------2/3");
+	}
+	//********************************************
 	
-	
-	
-	
-	//Tests for Given Program Input
-	
-	public void testInput1() {
-		//same as perfect
-		//throwBallRepeat(12, 10);
-		
-		game.throwBall("XXXXXXXXXXXX");
-		
+	//Tests for Given Program Input    ************************	
+	public void testInput1() {		
+		game.throwBall("XXXXXXXXXXXX");		
 		assertEquals(300, game.score());
 	}
-	public void testInput2() {
-		/*game.throwBall(9);
-		game.throwBall(0);
-		game.throwBall(9);
-		game.throwBall(0);
-		game.throwBall(9);
-		game.throwBall(0);
-		game.throwBall(9);
-		game.throwBall(0);
-		game.throwBall(9);
-		game.throwBall(0);
-		game.throwBall(9);
-		game.throwBall(0);
-		game.throwBall(9);
-		game.throwBall(0);
-		game.throwBall(9);
-		game.throwBall(0);
-		game.throwBall(9);
-		game.throwBall(0);
-		game.throwBall(9);
-		game.throwBall(0);*/
-		
+	public void testInput2() {	
 		game.throwBall("9-9-9-9-9-9-9-9-9-9-");
-		assertEquals(90, game.score());
-		
+		assertEquals(90, game.score());		
 	}
 	public void testInput3() {
 		//throwBallRepeat(21, 5);
 		game.throwBall("5/5/5/5/5/5/5/5/5/5/5");
 		assertEquals(150, game.score());		
 	}
-	public void testInput4() {
-		/*game.throwBall(10);
-		game.throwBall(7);
-		game.throwBall(3);
-		game.throwBall(9);
-		game.throwBall(0);
-		game.throwBall(10);
-		game.throwBall(0);
-		game.throwBall(8);
-		game.throwBall(8);
-		game.throwBall(2);
-		game.throwBall(0);
-		game.throwBall(6);
-		game.throwBall(10);
-		game.throwBall(10);
-		game.throwBall(10);
-		game.throwBall(8);
-		game.throwBall(1);*/
-		
+	public void testInput4() {		
 		game.throwBall("X7/9-X-88/-6XXX81");
 		assertEquals(167, game.score());
-	}
-	
-	//**********************************************************
-	
+	}	
+	//**********************************************************	
 	// helper
 	private void throwBallRepeat(int numRolls, int numPins) {
 		for (int i = 0; i < numRolls; i++) {
 			game.throwBall(numPins);
 		} // ends for
 	}
-	
-	
+	//*****************************
 
 } // End Class
